024ea2d9b50b469b2c525f37f6ded4811a5df450
Mayco Lazos <maycolazos25@gmail.com>
2023-07-10 20:50:23 -0300

    primmer commit

---
+++ DataAccess/TurnoDal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess
+{
+    public static class MonedasDAL
+    {
+
+        public static List<Turnos> GetAll()
+        {
+
+            List<Turnos> list = new List<Turnos>();
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Cnn"].ToString()))
+            {
+                conn.Open();
+
+                string sql = @"ListaTurnos";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(ObtenerListadoTurnos(reader));
+                }
+            }
+
+            return list;
+        }
+        public static Turnos ObtenerListadoTurnos(SqlDataReader reader)
+        {
+            Turnos turno = new Turnos();
+            turno.Horario = Convert.ToString(reader["horario"]);
+            turno.Disponible = Convert.ToBoolean(reader["disponible"]);
+           
+            return turno;
+        }
+    }
+        
+    }
+
---
+++ Entities/Turnos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Turnos
+    {
+        public int Id { get; set; }
+        public string Horario { get; set; }
+        public bool Disponible { get; set; }
+        public int IdCliente { get; set; }
+
+    }
+}
